5745917f920040eff85f10c9845f8a1d0702b50b
Markus Tuominen <markus.tuominen91@gmail.com>
2017-09-01 22:50:31 +0300

    mostly styles and colors

--- JavaIntervalTimer/src/javaintervaltimer/JavaIntervalTimer.java
+++ JavaIntervalTimer/src/javaintervaltimer/JavaIntervalTimer.java
@@ -1,5 +1,6 @@
 package javaintervaltimer;
 
+import java.awt.Color;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -11,7 +12,7 @@ public class JavaIntervalTimer extends javax.swing.JFrame {
 
     int restCounter = 0;
     int intervalCounter = 0;
-    
+
     //First a litlle bit of rest ro get ready for training
     public void startRestTimer() {
         Timer timer = new Timer();
@@ -23,6 +24,15 @@ public class JavaIntervalTimer extends javax.swing.JFrame {
                 timeField.setText(Integer.toString(restCounter));
                 System.out.println(Integer.toString(restCounter));
                 restCounter--;
+
+                if (restCounter < 20) {
+                    timeField.setForeground(Color.yellow);
+                }
+
+                if (restCounter < 10) {
+                    timeField.setForeground(Color.red);
+                }
+
                 if (restCounter <= -1) {
                     timer.cancel();
                     startIntervalTimer();
@@ -42,6 +52,19 @@ public class JavaIntervalTimer extends javax.swing.JFrame {
                 timeField.setText(Integer.toString(intervalCounter));
                 System.out.println(Integer.toString(intervalCounter));
                 intervalCounter--;
+                
+                if (intervalCounter > 20) {
+                    timeField.setForeground(Color.green);
+                }
+                
+                if (intervalCounter < 20) {
+                    timeField.setForeground(Color.yellow);
+                }
+
+                if (intervalCounter < 10) {
+                    timeField.setForeground(Color.red);
+                }
+
                 if (intervalCounter <= -1) {
                     timer.cancel();
                     startStopButton.setEnabled(true);
@@ -64,55 +87,63 @@ public class JavaIntervalTimer extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
         timeField = new javax.swing.JTextField();
         startStopButton = new javax.swing.JButton();
-        jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        jMenu2 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        timeField.setEditable(false);
-        timeField.setBackground(new java.awt.Color(255, 255, 255));
-        timeField.setFont(new java.awt.Font("Tahoma", 0, 48)); // NOI18N
-        timeField.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-        timeField.setToolTipText("");
+        jPanel1.setBackground(new java.awt.Color(51, 51, 51));
 
-        startStopButton.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        startStopButton.setText("|>");
+        timeField.setEditable(false);
+        timeField.setBackground(new java.awt.Color(0, 0, 0));
+        timeField.setFont(new java.awt.Font("Calibri", 0, 36)); // NOI18N
+        timeField.setForeground(new java.awt.Color(0, 255, 0));
+        timeField.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        timeField.setBorder(null);
+        timeField.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        timeField.setDisabledTextColor(new java.awt.Color(0, 0, 0));
+
+        startStopButton.setBackground(new java.awt.Color(0, 255, 0));
+        startStopButton.setFont(new java.awt.Font("Calibri", 0, 36)); // NOI18N
+        startStopButton.setText("Start");
+        startStopButton.setBorder(null);
         startStopButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 startStopButtonActionPerformed(evt);
             }
         });
 
-        jMenu1.setText("File");
-        jMenuBar1.add(jMenu1);
-
-        jMenu2.setText("Edit");
-        jMenuBar1.add(jMenu2);
-
-        setJMenuBar(jMenuBar1);
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(80, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(startStopButton, javax.swing.GroupLayout.PREFERRED_SIZE, 199, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(timeField, javax.swing.GroupLayout.PREFERRED_SIZE, 199, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(80, 80, 80))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(71, 71, 71)
+                .addComponent(timeField, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(startStopButton, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(113, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(90, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(timeField, javax.swing.GroupLayout.DEFAULT_SIZE, 206, Short.MAX_VALUE)
-                    .addComponent(startStopButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addGap(90, 90, 90))
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap(45, Short.MAX_VALUE)
-                .addComponent(timeField, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(startStopButton, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(28, 28, 28))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -156,9 +187,7 @@ public class JavaIntervalTimer extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
-    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JButton startStopButton;
     private javax.swing.JTextField timeField;
     // End of variables declaration//GEN-END:variables
